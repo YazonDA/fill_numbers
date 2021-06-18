1e496c97c9cbb674f9be1e7663c5dbb86d6969d6
YazonDA <yazonda.29@gmail.com>
2021-06-18 15:17:13 +0300

    bugfix: sql-request returned not list[str] but list[tuples]

--- modules/servers_base.py
+++ modules/servers_base.py
@@ -19,13 +19,13 @@ def lists_in_dbase():
 		# cursor for doing something
 		cursor = connection.cursor()
 
-		request_motes = f'SELECT to_hex(eui) FROM motes;'
-		request_lights = f'SELECT modem_dev_eui FROM lights;'
+		request_motes = f"SELECT lpad(to_hex(eui), 16, '0') FROM motes;"
+		request_lights = f"SELECT modem_dev_eui FROM lights;"
 
 		cursor.execute(request_motes)
-		_list_motes = cursor.fetchall()
+		_list_motes = list(map(lambda x: x[0], cursor.fetchall()))
 		cursor.execute(request_lights)
-		_list_lights = cursor.fetchall()
+		_list_lights = list(map(lambda x: x[0], cursor.fetchall()))
 		return (_list_lights, _list_motes)
 		logging.info(f'RETURN isn`t working')
 
@@ -47,9 +47,10 @@ def lists_in_dbase_from_file(_file_db_lights, _file_db_motes):
 def motes_no_lights(_df_4_fill, _file_db_lights, _file_db_motes):
 	logging.info(f'_df_4_fill {len(_df_4_fill)} , {type(_df_4_fill)}')
 	#_list_lights, _list_motes = lists_in_dbase_from_file(_file_db_lights, _file_db_motes)	
-	_list_lights, _list_motes = lists_in_dbase_()
+	_list_lights, _list_motes = lists_in_dbase()
 	logging.info(f'return from lists_in_dbase (_list_lights, _list_motes)')
 	logging.info(f'_list_lights {len(_list_lights)}, _list_motes {len(_list_motes)}')
+	logging.info(f'\nExample\n_list_lights {_list_lights[:2]}\n_list_motes {_list_motes[:2]}')
 
 	_df_err_not_motes = _df_4_fill[~_df_4_fill['DevEUI'].isin(_list_motes)]
 	_df_4_fill = _df_4_fill[_df_4_fill['DevEUI'].isin(_list_motes)]

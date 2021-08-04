521db5f0fb350f74399629bca9d453377849bce8
YazonDA <yazonda.29@gmail.com>
2021-08-04 16:07:50 +0300

    one-task special

--- modules/servers_base.py
+++ modules/servers_base.py
@@ -67,20 +67,10 @@ def get_table(dev_list):
 		connection = psycopg2.connect("dbname='customer_01' user='lorawan' host='localhost' password='ves2018'")
 		# cursor for doing something
 		cursor = connection.cursor()
-		request_table = f"SELECT \
-			lights.modem_dev_eui, poles.latitude, poles.longitude, poles.pole_id_original, poles.rfid, \
-			lk_sectors.name, poles.sector_id, street_refs.name, pole_refs.name, poles.lamp_marks, \
-			poles.okrug_municipal, poles.okrug_adm, poles.street, poles.pole_orientir \
-			FROM lights \
-			JOIN poles \
-			ON poles.pole_id = lights.pole_id \
-			JOIN pole_refs \
-			ON pole_refs.ref_id=poles.ref_id \
-			JOIN street_refs \
-			ON street_refs.ref_id=poles.street_ref_id \
-			JOIN lk_sectors \
-			ON lk_sectors.pk_sector_id=poles.sector_id \
-			WHERE lights.modem_dev_eui IN {tuple(dev_list)};"
+		request_table = f"SELECT\
+							lpad(to_hex(eui), 16, '0'), lpad(to_hex(appeui), 16, '0')\
+							FROM motes\
+							WHERE lpad(to_hex(eui), 16, '0') IN {tuple(dev_list)};"
 
 		cursor.execute(request_table)
 		_list_motes = list(cursor.fetchall())
@@ -94,31 +84,6 @@ def get_table(dev_list):
 			cursor.close()
 			connection.close()
 			logging.info(f"\nConnection to PostgreSQL is closed\n")
-
-
-def lists_in_dbase_from_file(_file_db_lights, _file_db_motes):
-	_list_lights = modules.read_csv(_file_db_lights)
-	_list_motes = modules.read_csv(_file_db_motes, True)
-	logging.info('it`s completed!')
-	return (_list_lights, _list_motes)
-
-def motes_no_lights(_df_4_fill, _file_db_lights=0, _file_db_motes=0):
-	#_list_lights, _list_motes = lists_in_dbase_from_file(_file_db_lights, _file_db_motes)	
-	_list_lights_true, _list_lights_false, _list_motes = lists_in_dbase()
-	
-	_df_err_not_motes = _df_4_fill[~_df_4_fill['DevEUI'].isin(_list_motes)]
-	_df_4_fill = _df_4_fill[_df_4_fill['DevEUI'].isin(_list_motes)]
-	_df_err_in_lights_true = _df_4_fill[_df_4_fill['DevEUI'].isin(_list_lights_true)]
-	_df_4_fill = _df_4_fill[~_df_4_fill['DevEUI'].isin(_list_lights_true)]
-	_df_err_in_lights_false = _df_4_fill[_df_4_fill['DevEUI'].isin(_list_lights_false)]
-	_df_4_fill = _df_4_fill[~_df_4_fill['DevEUI'].isin(_list_lights_false)]
-	logging.info(f'it`s completed!\n\
-		not_motes == {len(_df_err_not_motes)}\n\
-		in_lights_true == {len(_df_err_in_lights_true)}\n\
-		in_lights_false == {len(_df_err_in_lights_false)}\n\
-		4_fill == {len(_df_4_fill)}')
-
-	return (_df_err_not_motes, _df_err_in_lights_true, _df_err_in_lights_false, _df_4_fill)
 
 
 if __name__ == '__main__':

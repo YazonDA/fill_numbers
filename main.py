1218545d3e5876121b3e69088ed09b8d427bb446
YazonDA <yazonda.29@gmail.com>
2021-08-24 12:04:10 +0300

    bugfix: hide debugging code

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def main():
 	write_page_xlsx(df_err_in_lights_true, FILE_ERR_OUT, PAGE_ERR_ISIN)
 	write_new_xlsx(df_err_in_lights_false, FILE_4_REFILL)
 	#-6---------------------------------------------------------------
-	
+	'''
 	#>>>>>
 	write_new_xlsx(df_err_in_lights_false,'ERRinLIGHTSfalse')
 	zzz = df_err_in_lights_false['DevEUI'].tolist()
@@ -117,20 +117,19 @@ def main():
 	write_new_xlsx(zzz,'ZZZ')
 	df_err_in_lights_false = df_err_in_lights_false.sort_values(by='DevEUI')
 	df_err_in_lights_false = re_index(df_err_in_lights_false)
-	
-	return 0
-	
-	#<<<<<
 
+	return 0
+	#<<<<<
+	'''
 	# 12--
 	# Отделить "плохие" статусы
 	list_deveui = df_4_fill['DevEUI'].tolist()
 	list_wrong_stat, list_wrong_stat_self = check_stat(list_deveui)
 	write_page_xlsx(pd.DataFrame(list_wrong_stat_self, columns=['deveui', 'status']), FILE_ERR_OUT, 'WrongStat')
 	'''	DUPLICATE DATA
+	'''
 	df_err_bug_stat = df_4_fill[~df_4_fill['DevEUI'].isin(list_wrong_stat)]
 	write_new_xlsx(df_err_bug_stat, FILE_WR_STAT)
-	'''
 	df_4_fill = df_4_fill[df_4_fill['DevEUI'].isin(list_wrong_stat)]
 	#-13---------------------------------------------------------------
 
